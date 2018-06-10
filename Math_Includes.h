86e070ac57b11d65d79a659a7df11e86244916ae
aaronappel <aaron.appel@outlook.com>
2018-06-10 11:21:44 -0400

    Initial commit with assets and libraries/utilities. -Aaron

---
+++ Math_Includes.h
@@ -0,0 +1,17 @@
+#ifndef _Math_Includes_H_
+#define _Math_Includes_H_
+
+#include "Libraries/Math/MathConstants.h"
+#include "Libraries/Math/MathFunctions.h"
+#include "Libraries/Math/MathHelpers.h"
+#include "Libraries/Math/MathStuff.h"
+
+#include "Libraries/Math/VectorMathHelpers.h"
+#include "Libraries/Math/ValueChecking.h"
+// quaternion
+#include "Libraries/Math/MyQuaternion.h"
+// matrix
+#include "Libraries/Math/MyMatrix.h"
+typedef MyMatrix mat4;
+
+#endif // !_Math_Includes_H_
